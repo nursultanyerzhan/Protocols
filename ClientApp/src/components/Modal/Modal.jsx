4c1dabea6f5616968b48f5c737fc332c5398f758
nursultanyerzhan <nursultan_erzhan@mail.ru>
2023-01-08 00:01:13 +0600

    form

--- ClientApp/src/components/Modal/Modal.jsx
+++ ClientApp/src/components/Modal/Modal.jsx
@@ -1,7 +1,8 @@
 import style from './Modal.module.css';
 import ReactDOM from 'react-dom';
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import { useParams, useNavigate, useLocation } from 'react-router-dom';
+import { Button, Form, Col, InputGroup, Row } from 'react-bootstrap';
 
 export const Modal = () => {
   const overlayRef = useRef(null);
@@ -22,6 +23,37 @@ export const Modal = () => {
     };
   }, []);
 
+  async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
+
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    event.preventDefault();
+    if (form.checkValidity() === false) {
+
+      event.stopPropagation();
+    }
+    const formData = new FormData(form);
+    console.log(Object.fromEntries(formData));
+    const formObject = Object.fromEntries(formData);
+    postData('postTestData', formObject)
+      .then((data) => {
+        console.log(data); // JSON data parsed by `data.json()` call
+      });
+    setValidated(true);
+  };
+
   return ReactDOM.createPortal(
     <div className={style.overlay} ref={overlayRef}>
       <div className={style.modal}>
@@ -31,8 +63,80 @@ export const Modal = () => {
           </h2>
 
           <div className={style.content}>
-            <input />
-            <button>asdasdasd</button>
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+              <Row className="mb-3">
+                <Form.Group as={Col} md="4" controlId="validationCustom01">
+                  <Form.Label>First name</Form.Label>
+                  <Form.Control
+                    required
+                    type="text"
+                    placeholder="First name"
+                    defaultValue="Mark"
+                    name='FirstName'
+                  />
+                  <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group as={Col} md="4" controlId="validationCustom02">
+                  <Form.Label>Last name</Form.Label>
+                  <Form.Control
+                    required
+                    type="text"
+                    placeholder="Last name"
+                    defaultValue="Otto"
+                    name='LastName'
+                  />
+                  <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+                  <Form.Label>Username</Form.Label>
+                  <InputGroup hasValidation>
+                    <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                    <Form.Control
+                      type="text"
+                      placeholder="Username"
+                      aria-describedby="inputGroupPrepend"
+                      required
+                      name='Username'
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Please choose a username.
+                    </Form.Control.Feedback>
+                  </InputGroup>
+                </Form.Group>
+              </Row>
+              <Row className="mb-3">
+                <Form.Group as={Col} md="6" controlId="validationCustom03">
+                  <Form.Label>City</Form.Label>
+                  <Form.Control type="text" placeholder="City" required name='City' />
+                  <Form.Control.Feedback type="invalid">
+                    Please provide a valid city.
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group as={Col} md="3" controlId="validationCustom04">
+                  <Form.Label>State</Form.Label>
+                  <Form.Control type="text" placeholder="State" required />
+                  <Form.Control.Feedback type="invalid">
+                    Please provide a valid state.
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group as={Col} md="3" controlId="validationCustom05">
+                  <Form.Label>Zip</Form.Label>
+                  <Form.Control type="text" placeholder="Zip" required />
+                  <Form.Control.Feedback type="invalid">
+                    Please provide a valid zip.
+                  </Form.Control.Feedback>
+                </Form.Group>
+              </Row>
+              <Form.Group className="mb-3">
+                <Form.Check
+                  required
+                  label="Agree to terms and conditions"
+                  feedback="You must agree before submitting."
+                  feedbackType="invalid"
+                />
+              </Form.Group>
+              <Button type="submit">Submit form</Button>
+            </Form>
           </div>
         </>
       </div>
--- Controllers/ProtocolDocumentController.cs
+++ Controllers/ProtocolDocumentController.cs
@@ -1,3 +1,4 @@
+using BrunoZell.ModelBinding;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Protocols.DB;
@@ -21,4 +22,37 @@ public class ProtocolDocumentController : ControllerBase
             .ToListAsync();
         return Ok(list);
     }
+
+    [Route("postTestData")]
+    [HttpPost]
+    public async Task<ActionResult> PostTestData([FromForm] IFormFile file) //, List<IFormFile> file
+    {
+        var list = await _context.ProtocolDocuments
+            .ToListAsync();
+        return Ok(list);
+    }
+
+    [Route("uploadphoto")]
+    [HttpPost]
+    public string UploadPhoto([FromForm] DtoTestData data) //
+    {
+        return "file.Name0000000000000000000000000000000";
+    }
+}
+
+public class DtoTestData
+{
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    public string username { get; set; }
+    public string city { get; set; }
+    public string state { get; set; }
+    public string zip { get; set; }
+    public bool terms { get; set; }
+    public IFormFile File { get; set; }
+}
+
+public class UploadPhotoRequest
+{
+    public IFormFile File { get; set; }
 }
